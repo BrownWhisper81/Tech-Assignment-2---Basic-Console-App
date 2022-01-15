6e7455bf5d168364d37050d864fb0324158d05e5
caleb <caleb@DESKTOP-U5R9LOP>
2022-01-15 14:30:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Programmer: Caleb Brown
+using System;
+
+namespace Tech_Assignment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Roses are red, Violets are blue, my name is Caleb. How are you?");
+        }
+    }
+}
